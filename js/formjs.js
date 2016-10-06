5604f4666aa282fb43d5541e3a5a8520a4be1cf4
j-halloran <jakehalloran2@gmail.com>
2016-10-06 18:59:18 -0400

    Started javascript

--- js/formjs.js
+++ js/formjs.js
@@ -1,5 +1,20 @@
 $(document).ready(function(){
   $("#harambeForm").submit(function(event){
+    if(verifyForm()){
+      $("#formHolder").hide();
+      $("#errorHolder").hide();
+      $("#successHolder").show();
+    }
+    else{
+      $("#errorHolder").show();
+    }
     return false;
   });
 });
+
+function verifyForm(){
+  $("#")
+  if(!$("input[name='meme-button']:checked").val()){
+    return false;
+  }
+}
