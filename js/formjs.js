797d7276954c2d34fe90f2702c69baccb8547230
j-halloran <jakehalloran2@gmail.com>
2016-10-06 19:53:03 -0400

    Completed js/jquery scripts

--- js/formjs.js
+++ js/formjs.js
@@ -1,4 +1,8 @@
 $(document).ready(function(){
+
+  //Hide the error messages to start
+  $("#errorHolder").hide();
+
   $("#harambeForm").submit(function(event){
     if(verifyForm()){
       $("#formHolder").hide();
@@ -13,8 +17,27 @@ $(document).ready(function(){
 });
 
 function verifyForm(){
-  $("#")
+  //Hide each individual error
+  $("#nameError").hide();
+  $("#emailError").hide();
+  $("#buttonError").hide();
+  $("#emailFormatError").hide();
+
   if(!$("input[name='meme-button']:checked").val()){
+    $("#buttonError").show();
     return false;
   }
+  else if($("input[name='name-field']").val().length==0){
+    $("#nameError").show();
+    return false;
+  }
+  else if($("input[name='email-field']").val().length==0){
+    $("#emailError").show();
+    return false;
+  }
+  else if($("input[name='email-field']").val().indexOf('@')==-1){
+    $("#emailFormatError").show();
+    return false;
+  }
+  return true;
 }
